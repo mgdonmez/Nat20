e29487f20c8db7c845ed3653e70aeff551d1fcb0
Merve Gonca Dönmez <mgoncadonmez@gmail.com>
2025-01-26 16:48:24 +0300

    Host and piechart updated

--- Assets/Scripts/PieChart.cs
+++ Assets/Scripts/PieChart.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PieChart : MonoBehaviour
 {
-    
+    [SerializeField]
+    GameObject[] slices;
+
+    private void FixedUpdate()
+    {
+        SetStats();
+    }
+
+    public void SetStats()
+    {
+        slices[0].GetComponent<Image>().fillAmount = Manager.Instance.Population[(int)DemographicType.Square].Ratio;
+        slices[1].GetComponent<Image>().fillAmount = Manager.Instance.Population[(int)DemographicType.Triangle].Ratio;
+    }
+
+
 }
